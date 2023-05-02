75dbdf12d08929eb3633272ab5e6ba9a593546dd
Vikas Kumar Jaiswal <77603563+Mr-Vikas02@users.noreply.github.com>
2023-05-02 12:42:51 +0530

    Update FtechizApplication.java

--- ftechiz/src/main/java/com/ftechiz/project/FtechizApplication.java
+++ ftechiz/src/main/java/com/ftechiz/project/FtechizApplication.java
@@ -10,22 +10,4 @@ public class FtechizApplication {
 		SpringApplication.run(FtechizApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner initData(ContactRepository contactRepository) {
-//		return args -> {
-//			Address address1 = new Address();
-//			address1.setAddressLine1("123 Main St");
-//			address1.setAddressLine2("Apt 4");
-//			address1.setCity("Anytown");
-//			address1.setPostalCode("12345");
-//
-//			Contact contact1 = new Contact();
-//			contact1.setFullName("John Doe");
-//			contact1.setDateOfBirth(LocalDate.of(1980, 1, 1));
-//			contact1.setAddress(address1);
-//
-//			contactRepository.save(contact1);
-//		};
-//	}
-
 }
